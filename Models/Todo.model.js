3662380a47701c6a5f4b79dff06b75febe96f58e
Param1Raj <pr6587424@gmail.com>
2022-12-12 18:32:18 +0530

    done

--- Models/Todo.model.js
+++ Models/Todo.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const TodoModel = mongoose.model("Todo",{
     taskname:String,
     status:String,
-    tag:String
+    tag:String,
+    user_id:String
 })
 
 module.exports = {TodoModel};
